3861cc383ac73e0d4db850b20b7eccd107d25a9e
chasingzen <jimmy.starling@gmail.com>
2025-04-03 21:30:02 -0400

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import EchoTraceHomePage from '../components/EchoTraceHomePage';
+import dynamic from 'next/dynamic';
+const EchoTraceHomePage = dynamic(() => import('../components/EchoTraceHomePage'), { ssr: false });
 
 export default function Home() {
   return <EchoTraceHomePage />;
