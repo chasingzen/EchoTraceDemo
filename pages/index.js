d721ec10e4f457a061884903c5adad2072dd3352
chasingzen <jimmy.starling@gmail.com>
2025-04-03 22:16:47 -0400

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const EchoTraceHomePage = dynamic(() => import('../components/EchoTraceHomePage'), { ssr: false });
-
-export default function Home() {
-  return <EchoTraceHomePage />;
-}
